813112208b9ea02438b038657ba129f57fde6040
Kamil Paduszyński <paduszyk@gmail.com>
2023-11-02 18:19:51 +0100

    🥅 Handle errors due to invalid API response JSON format

--- src/gitmojis/core.py
+++ src/gitmojis/core.py
@@ -2,6 +2,7 @@ from typing import Final
 
 import requests
 
+from .exceptions import ResponseJsonError
 from .model import Gitmoji, Guide
 
 GITMOJI_API_URL: Final = "https://gitmoji.dev/api/gitmojis"
@@ -12,7 +13,8 @@ GITMOJI_API_KEY: Final = "gitmojis"
 def fetch_guide() -> Guide:
     response = requests.get(GITMOJI_API_URL)
 
-    gitmojis_json = response.json()[GITMOJI_API_KEY]
+    if (gitmojis_json := response.json().get(GITMOJI_API_KEY)) is None:
+        raise ResponseJsonError
 
     guide = Guide(gitmojis=[Gitmoji(**gitmoji_json) for gitmoji_json in gitmojis_json])
 
--- tests/test_core.py
+++ tests/test_core.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 
 from gitmojis.core import GITMOJI_API_KEY, fetch_guide
+from gitmojis.exceptions import ResponseJsonError
 from gitmojis.model import Guide
 
 
@@ -18,3 +19,12 @@ def test_fetch_guide_creates_guide_from_api_response_json(mocker, response):
     guide = fetch_guide()
 
     assert isinstance(guide, Guide)
+
+
+def test_fetch_guide_raises_error_if_gitmoji_api_key_not_in_response_json(mocker, response):  # fmt: skip
+    response.json.return_value = {}  # `GITMOJI_API_KEY` not in the response's JSON
+
+    mocker.patch("requests.get", return_value=response)
+
+    with pytest.raises(ResponseJsonError):
+        fetch_guide()
